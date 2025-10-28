54244bf6119e9d18d416fe4ccc0e566b655f3443
learning909-tech <lopam@MacBook-Air.local>
2025-10-28 12:54:05 +0530

    edited app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          My 1st Prod Deployment!
         </p>
         <a
           className="App-link"
